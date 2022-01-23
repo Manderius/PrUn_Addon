95e2740dcb28911ab6176228fd6fc17c96aa303f
Manderius <manderiusmail@gmail.com>
2022-01-23 13:02:22 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# PrUn_Addon
+# Screen addon for Prosperous Universe
 TamperMonkey script to improve UI of Prosperous Universe.
+This script adds individual screens from the SCRNS dropdown to the top bar next to the **FULL** button.
+
+Copy and paste contents of `addon.js` into a new TamperMonkey script and save, then reload Prosperous Universe.
---
+++ addon.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name         Prosperous Universe
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Prosperous Universe enhancements.
+// @author       Manderius
+// @match        https://apex.prosperousuniverse.com/
+// @icon         https://www.google.com/s2/favicons?domain=prosperousuniverse.com
+// @grant        none
+// @downloadURL  https://github.com/Manderius/PrUn_Addon/main/addon.js
+// ==/UserScript==
+
+// Add exceptions that you don't want to be expanded
+// If you want all screens, use the following line instead:
+// const exceptions = [];
+const exceptions = ['finances'];
+
+function showScreensInTopBar() {
+    var navbar = document.getElementById('TOUR_TARGET_SCREEN_CONTROLS');
+    var navbarItemClassList = navbar.children[2].classList;
+    var nbitMainCL = navbar.children[2].children[0].classList;
+    var nbitUnderlineCL = navbar.children[2].children[1].classList;
+    var menuUl = navbar.children[1].children[1];
+    var links = [];
+    menuUl.childNodes.forEach((cn) => links.push({'Name' : cn.children[1].innerHTML, 'Link' : cn.children[1].href}));
+    for (var link of links) {
+        if (exceptions.includes(link.Name.toLowerCase())) continue;
+        var div = document.createElement('div');
+        div.classList = navbarItemClassList;
+        var aelem = document.createElement('a');
+        aelem.classList = nbitMainCL;
+        aelem.style.color = 'inherit';
+        aelem.innerHTML = link.Name;
+        aelem.href = link.Link;
+        div.appendChild(aelem);
+        var line = document.createElement('div');
+        line.classList = nbitUnderlineCL;
+        div.appendChild(line);
+        navbar.appendChild(div);
+    }
+}
+
+(function() {
+    'use strict';
+    setTimeout(showScreensInTopBar, 4000);
+})();
